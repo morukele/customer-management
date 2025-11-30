e24c9d7cdae3a6f44ecd161457dae5827308a968
Oghenemarho Orukele <orukele.dev@gmail.com>
2025-11-30 21:21:06 +0100

    feat: added enumerator decorator

--- cm_lib/source/data/enumerator-decorator.cpp
+++ cm_lib/source/data/enumerator-decorator.cpp
@@ -1,5 +1,76 @@
 #include "enumerator-decorator.h"
 
-EnumeratorDecorator::EnumeratorDecorator(QObject *parent)
-    : QObject{parent}
-{}
+namespace cm {
+namespace data {
+
+    class EnumeratorDecorator::Implementation
+    {
+    public:
+        Implementation(
+            EnumeratorDecorator* _enumeratorDecorator,
+            const int& _value,
+            const std::map<int, QString>& _descriptionMapper
+        )
+            : enumeratorDecorator(_enumeratorDecorator)
+            , value(_value)
+            , descriptionMapper(_descriptionMapper)
+        {
+        }
+
+        EnumeratorDecorator* enumeratorDecorator{nullptr};
+        int value;
+        std::map<int, QString> descriptionMapper;
+    };
+
+    EnumeratorDecorator::EnumeratorDecorator(
+        Entity* parentEntity,
+        const QString& key,
+        const QString& label,
+        int value,
+        const std::map<int, QString>& descriptionMapper
+        )
+        : DataDecorator(parentEntity, key, label)
+    {
+        implementation.reset(new Implementation(this, value, descriptionMapper));
+    }
+
+    EnumeratorDecorator::~EnumeratorDecorator()
+    {
+    }
+
+    int EnumeratorDecorator::value() const
+    {
+        return implementation->value;
+    }
+
+    QString EnumeratorDecorator::valueDescription() const
+    {
+        // .find(key) returns an end() if "key" not found in map.
+        if (implementation->descriptionMapper.find(implementation->value)
+            != implementation->descriptionMapper.end()) {
+            return implementation->descriptionMapper.at(implementation->value);
+        } else {
+            return {};
+        }
+    }
+
+    EnumeratorDecorator& EnumeratorDecorator::setValue(const int& value) {
+        if (value != implementation->value) {
+            implementation->value = value;
+            emit valueChanged();
+        }
+        return *this;
+    }
+
+    QJsonValue EnumeratorDecorator::jsonValue() const {
+        return QJsonValue::fromVariant(QVariant(implementation->value));
+    }
+
+    void EnumeratorDecorator::update(const QJsonObject& _jsonObject) {
+        if (_jsonObject.contains(key())) {
+            setValue(_jsonObject.value(key()).toInt());
+        } else {
+            setValue(0);
+        }
+    }
+}}
--- cm_lib/source/data/enumerator-decorator.h
+++ cm_lib/source/data/enumerator-decorator.h
@@ -1,15 +1,47 @@
 #ifndef ENUMERATOR_DECORATOR_H
 #define ENUMERATOR_DECORATOR_H
 
+#include <map>
+
 #include <QObject>
+#include <QJsonObject>
+#include <QJsonValue>
+#include <QScopedPointer>
 
-class EnumeratorDecorator : public QObject
-{
-    Q_OBJECT
-public:
-    explicit EnumeratorDecorator(QObject *parent = nullptr);
+#include <cm_lib_global.h>
+#include <data/data-decorator.h>
 
-signals:
-};
+namespace cm {
+namespace data {
+    class CM_LIB_EXPORT EnumeratorDecorator : public DataDecorator
+    {
+        Q_OBJECT
+        Q_PROPERTY(int ui_value READ value WRITE setValue NOTIFY valueChanged)
+        Q_PROPERTY(QString ui_valueDescription READ valueDescription NOTIFY valueChanged)
+    public:
+        EnumeratorDecorator(
+            Entity* parentEntity = nullptr,
+            const QString& key = "SomeItemKey",
+            const QString& label = "",
+            int value = 0,
+            const std::map<int, QString>& descriptionMapper = std::map<int, QString>()
+        );
+        ~EnumeratorDecorator();
+
+        EnumeratorDecorator& setValue(const int& value);
+        int value() const;
+        QString valueDescription() const;
+
+        QJsonValue jsonValue() const override;
+        void update(const QJsonObject& jsonObject) override;
+
+    signals:
+        void valueChanged();
+
+    private:
+        class Implementation;
+        QScopedPointer<Implementation> implementation;
+    };
+}}
 
 #endif // ENUMERATOR_DECORATOR_H
