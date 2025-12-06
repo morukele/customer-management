0969bb01a3c287b8e801825619e9d96b1e9e81de
Oghenemarho Orukele <orukele.dev@gmail.com>
2025-12-06 09:44:00 +0100

    feat: added support for client and all input fields

--- cm_lib/source/models/contact.cpp
+++ cm_lib/source/models/contact.cpp
@@ -1,5 +1,28 @@
 #include "contact.h"
 
-Contact::Contact(QObject *parent)
-    : QObject{parent}
-{}
+using namespace cm::data;
+
+namespace cm {
+namespace models {
+    Contact::Contact(QObject *parent)
+    : Entity(parent, "contact")
+    {
+        contactType = static_cast<EnumeratorDecorator*>(addDataItem(
+            new EnumeratorDecorator(this, "contactType", "Contact Type", 0, contactTypeMapper)));
+        address = static_cast<StringDecorator*>(addDataItem(new StringDecorator(this, "address", "Address")));
+    }
+
+    Contact::Contact(QObject* parent, const QJsonObject& json)
+        : Contact(parent)
+    {
+        update(json);
+    }
+
+    std::map<int, QString> Contact::contactTypeMapper = std::map<int, QString>
+    {
+        {Contact::eContactType::Unknown, ""},
+        {Contact::eContactType::Telephone, "Telephone"},
+        {Contact::eContactType::Email, "Email"},
+        {Contact::eContactType::Fax, "Fax"}
+    };
+}}
--- cm_lib/source/models/contact.h
+++ cm_lib/source/models/contact.h
@@ -2,14 +2,39 @@
 #define CONTACT_H
 
 #include <QObject>
+#include <data/string-decorator.h>
+#include <data/enumerator-decorator.h>
+#include <data/entity.h>
 
-class Contact : public QObject
-{
-    Q_OBJECT
-public:
-    explicit Contact(QObject *parent = nullptr);
+#include <cm_lib_global.h>
 
-signals:
-};
+namespace cm {
+namespace models {
+
+class CM_LIB_EXPORT Contact : public data::Entity
+    {
+        Q_OBJECT
+        Q_PROPERTY(cm::data::EnumeratorDecorator* ui_contactType MEMBER contactType CONSTANT)
+        Q_PROPERTY(cm::data::StringDecorator* ui_address MEMBER address CONSTANT)
+
+    public:
+        enum eContactType {
+            Unknown = 0,
+            Telephone,
+            Email,
+            Fax
+        };
+
+    public:
+        explicit Contact(QObject *parent = nullptr);
+        Contact(QObject *parent, const QJsonObject& json);
+
+        data::EnumeratorDecorator* contactType{nullptr};
+        data::StringDecorator* address{nullptr};
+        static std::map<int, QString> contactTypeMapper;
+
+    signals:
+    };
+}}
 
 #endif // CONTACT_H
