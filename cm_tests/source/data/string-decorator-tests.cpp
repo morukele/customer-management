b14d9a01e9e8f8492cdb884b3135275f7f43bd39
Oghenemarho Orukele <orukele.dev@gmail.com>
2025-12-11 16:59:41 +0100

    finished string-decorator test

--- cm_tests/source/data/string-decorator-tests.cpp
+++ cm_tests/source/data/string-decorator-tests.cpp
@@ -1,3 +1,97 @@
 #include "string-decorator-tests.h"
 
-StringDecoratorTests::StringDecoratorTests() {}
+#include <QSignalSpy>
+
+#include <data/entity.h>
+
+namespace cm {
+namespace data {
+    // Instance
+    static StringDecoratorTests instance;
+
+    StringDecoratorTests::StringDecoratorTests()
+        : TestSuite("StringDecoratorTests")
+    {
+    }
+
+    // Tests
+    void StringDecoratorTests::constructor_givenNoParameters_setsDefaultProperties()
+    {
+        StringDecorator decorator;
+        QCOMPARE(decorator.parentEntity(), nullptr);
+        QCOMPARE(decorator.key(), QString("SomeItemKey"));
+        QCOMPARE(decorator.label(), QString(""));
+        QCOMPARE(decorator.value(), QString(""));
+    }
+
+    void StringDecoratorTests::constructor_givenParameters_setsProperties()
+    {
+        Entity parentEntity;
+        StringDecorator decorator(&parentEntity, "Test Key", "Test Label", "Test Value");
+        QCOMPARE(decorator.parentEntity(), &parentEntity);
+        QCOMPARE(decorator.key(), QString("Test Key"));
+        QCOMPARE(decorator.label(), QString("Test Label"));
+        QCOMPARE(decorator.value(), QString("Test Value"));
+    }
+
+    void StringDecoratorTests::setValue_givenNewValue_updatesValueAndEmitsSignal()
+    {
+        StringDecorator decorator;
+        QSignalSpy valueChangedSpy(&decorator, &StringDecorator::valueChanged);
+        QCOMPARE(decorator.value(), QString(""));
+        decorator.setValue(QString("New Value"));
+        QCOMPARE(decorator.value(), QString("New Value"));
+        QCOMPARE(valueChangedSpy.count(), 1);
+    }
+
+    void StringDecoratorTests::setValue_givenSameValue_takesNoAction()
+    {
+        Entity parentEntity;
+        StringDecorator decorator(&parentEntity, "Test Key", "Test Label", "Test Value");
+        QSignalSpy valueChangedSpy(&decorator, &StringDecorator::valueChanged);
+        QCOMPARE(decorator.value(), QString("Test Value"));
+        decorator.setValue("Test Value");
+        QCOMPARE(decorator.value(), QString("Test Value"));
+        QCOMPARE(valueChangedSpy.count(), 0);
+    }
+
+    void StringDecoratorTests::jsonValue_whenDefaultValue_returnsJson() {
+        StringDecorator decorator;
+        QCOMPARE(decorator.jsonValue(), QJsonValue(QString("")));
+    }
+
+    void StringDecoratorTests::jsonValue_whenValueSet_returnsJson() {
+        StringDecorator decorator;
+        decorator.setValue(QString("New Value"));
+        QCOMPARE(decorator.jsonValue(), QJsonValue("New Value"));
+    }
+
+    void StringDecoratorTests::update_whenPresentInJson_updatesValue()
+    {
+        Entity parentEntity;
+        StringDecorator decorator(&parentEntity, "Test Key", "Test Label", "Test Value");
+        QSignalSpy valueChangedSpy(&decorator, &StringDecorator::valueChanged);
+        QCOMPARE(decorator.value(), QString("Test Value"));
+        QJsonObject jsonObject;
+        jsonObject.insert("Key 1", "Value 1");
+        jsonObject.insert("Key 2", 123);
+        jsonObject.insert("Test Key", "New Test Value");
+        decorator.update(jsonObject);
+        QCOMPARE(decorator.value(), QString("New Test Value"));
+        QCOMPARE(valueChangedSpy.count(), 1);
+    }
+
+    void StringDecoratorTests::update_whenNotPresentInJson_updatesValueToDefault() {
+        Entity parentEntity;
+        StringDecorator decorator(&parentEntity, "Test Key", "Test Label", "Test Value");
+        QSignalSpy valueChangedSpy(&decorator, &StringDecorator::valueChanged);
+        QCOMPARE(decorator.value(), QString("Test Value"));
+        QJsonObject jsonObject;
+        jsonObject.insert("Key 1", "Value 1");
+        jsonObject.insert("Key 2", 123);
+        jsonObject.insert("Key 3", "should not work");
+        decorator.update(jsonObject);
+        QCOMPARE(decorator.value(), QString(""));
+        QCOMPARE(valueChangedSpy.count(), 1);
+    }
+}}
--- cm_tests/source/data/string-decorator-tests.h
+++ cm_tests/source/data/string-decorator-tests.h
@@ -1,10 +1,29 @@
 #ifndef STRING_DECORATOR_TESTS_H
 #define STRING_DECORATOR_TESTS_H
 
-class StringDecoratorTests
-{
-public:
-    StringDecoratorTests();
-};
+#include <QtTest>
+
+#include <data/string-decorator.h>
+#include <test-suite.h>
+
+namespace cm {
+namespace data {
+    class StringDecoratorTests : public TestSuite
+    {
+        Q_OBJECT
+    public:
+        StringDecoratorTests();
+
+    private slots:
+        void constructor_givenNoParameters_setsDefaultProperties();
+        void constructor_givenParameters_setsProperties();
+        void setValue_givenNewValue_updatesValueAndEmitsSignal();
+        void setValue_givenSameValue_takesNoAction();
+        void jsonValue_whenDefaultValue_returnsJson();
+        void jsonValue_whenValueSet_returnsJson();
+        void update_whenPresentInJson_updatesValue();
+        void update_whenNotPresentInJson_updatesValueToDefault();
+    };
+}}
 
 #endif // STRING_DECORATOR_TESTS_H
