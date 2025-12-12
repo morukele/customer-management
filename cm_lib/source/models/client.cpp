da3e70890bca98db62a18b84a81b39326adf9a50
Oghenemarho Orukele <orukele.dev@gmail.com>
2025-12-12 23:03:54 +0100

    added test for primary key functionality

--- cm_lib/source/models/client.cpp
+++ cm_lib/source/models/client.cpp
@@ -13,10 +13,12 @@ namespace models {
         billingAddress = static_cast<Address*>(addChild(new Address(this), "billingAddress"));
         appointments = static_cast<EntityCollection<Appointment>*>(
             addChildCollection(new EntityCollection<Appointment>(this, "appointments"))
-            );
+        );
         contacts = static_cast<EntityCollection<Contact>*>(
             addChildCollection(new EntityCollection<Contact>(this, "contacts"))
-            );
+        );
+
+        setPrimaryKey(reference);
     }
 
     Client::Client(QObject* parent, const QJsonObject& json)
